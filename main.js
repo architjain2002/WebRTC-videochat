60478b5ad94a7b9680fe34173acfcc39908d8c72
Archit Jain <arcyjain2002@gmail.com>
2022-07-10 23:26:26 +0530

    updated the main js

--- main.js
+++ main.js
@@ -8,6 +8,20 @@ let init = async () => {
   });
 
   document.getElementById("user-1").srcObject = localStream;
+
+  createOffer();
 };
 
 init();
+
+let createOffer = async () => {
+  peerConnection = new RTCPeerConnection();
+
+  remoteStream = new MediaStream();
+
+  document.getElementById("user-2").srcObject = remoteStream;
+
+  let offer = await peerConnection.createOffer();
+  await peerConnection.setLocalDescription(offer);
+  console.log(offer);
+};
