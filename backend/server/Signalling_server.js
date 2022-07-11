025816c9cdc93161efdc899d42e73aa99e0583fa
Archit Jain <arcyjain2002@gmail.com>
2022-07-12 02:03:02 +0530

    created the room for the client

---
+++ backend/server/Signalling_server.js
@@ -0,0 +1,23 @@
+const io = require("socket.io");
+
+io.sockets.on("connection", (socket) => {
+  // when a client creates or joins a room in client side
+  socket.on("create or join", (room) => {
+    // io.sockets.clients gives the number of connections in a room
+    const numberOfClients = io.sockets.clients(room).length();
+
+    if (numberOfClients === 0) {
+      socket.join(room);
+      socket.emit("created", room);
+    }
+
+    if (numberOfClients == 1) {
+      //this will emit the message to whoever is present in the room (here we have only one client)
+      io.to(room).emit("join", room);
+      socket.join(room);
+      socket.emit("joined", room);
+    } else {
+      socket.emit("full", room);
+    }
+  });
+});
