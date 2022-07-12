6cc055ee373e55e9984392cb2303cbf13dea68ee
Archit Jain <arcyjain2002@gmail.com>
2022-07-12 16:51:39 +0530

    connected the exported socketio

--- backend/index.js
+++ backend/index.js
@@ -4,9 +4,12 @@ const express = require("express");
 const app = express();
 const file = new static.Server();
 require("dotenv").config();
-
 const server = http.createServer(app);
+const io = require("socket.io")(server);
 const port = process.env.PORT || 8000;
+
+const singallingServer = require("./server/signalling_server.js")(io); //
+frontend / main.js;
 server.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
