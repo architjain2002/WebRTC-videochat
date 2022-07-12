2fbd66f9620aba256891543a45b594e4cecf1a91
Archit Jain <arcyjain2002@gmail.com>
2022-07-12 16:11:37 +0530

    added dotenv

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,12 @@
 const static = require("node-static");
 const http = require("http");
-
+const express = require("express");
+const app = express();
 const file = new static.Server();
+require("dotenv").config();
+
+const server = http.createServer(app);
+const port = process.env.PORT || 8000;
+server.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
