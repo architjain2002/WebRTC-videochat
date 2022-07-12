dab3e36e086e170cec37b5ce135e34bf9fb81a07
Archit Jain <arcyjain2002@gmail.com>
2022-07-13 01:59:22 +0530

    handled all events

--- frontend/rooms.js
+++ frontend/rooms.js
@@ -1,6 +1,6 @@
 room = prompt("Enter your room name:");
 
-const socket = io("http://127.0.0.1:80"); // this will connect to backend which is currently being server on port 80
+export const socket = io("http://127.0.0.1:80"); // this will connect to backend which is currently being server on port 80
 socket.on("connect", () => {
   console.log(socket.id);
 });
