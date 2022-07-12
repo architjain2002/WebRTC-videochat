3b59267c63bcec949effab01b4ed5119496760d2
Archit Jain <arcyjain2002@gmail.com>
2022-07-12 17:09:23 +0530

    added cors

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 const static = require("node-static");
 const http = require("http");
 const express = require("express");
+const cors = require("cors"); // used to allow request from other localhost in this case // testing
 const app = express();
 const file = new static.Server();
 require("dotenv").config();
@@ -8,8 +9,7 @@ const server = http.createServer(app);
 const io = require("socket.io")(server);
 const port = process.env.PORT || 8000;
 
-const singallingServer = require("./server/signalling_server.js")(io); //
-frontend / main.js;
+const singallingServer = require("./server/signalling_server.js")(io);
 server.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
