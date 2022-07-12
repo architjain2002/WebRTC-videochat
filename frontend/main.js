c453056eb23c6869e63d529b7591fa49e14e9ec8
Archit Jain <arcyjain2002@gmail.com>
2022-07-13 01:59:06 +0530

    created connections between remote and local peers

--- frontend/main.js
+++ frontend/main.js
@@ -1,6 +1,7 @@
 let localStream;
 let remoteStream;
 
+import socket from "./rooms.js";
 // ice servers for candidates here i have considered all the stun servers
 const servers = {
   iceServers: [
@@ -11,7 +12,6 @@ const servers = {
 };
 
 // to access the video and the audio streams into localstream
-
 let init = async () => {
   localStream = await navigator.mediaDevices.getUserMedia({
     video: true,
@@ -28,6 +28,7 @@ init(); //invoke
 let createOffer = async () => {
   peerConnection = new RTCPeerConnection(servers); // we create connection by passing all the ice candidates
 
+  
   remoteStream = new MediaStream();
   document.getElementById("user-2").srcObject = remoteStream;
 
@@ -41,14 +42,44 @@ let createOffer = async () => {
     }); // to add the tracks to the remote stream on when tracks area added to the peer connection
   };
 
-  // onicecandidates is event is called then we just check
+  // onicecandidates is event is called 
   peerConnection.onicecandidate = (event) => {
     if (event.candidate) {
-      console.log(event.candidate);
+      socket.emit("candidate", event.candidate);
     }
   };
+  
+  // to create and send the offer
+  await peerConnection.createOffer((offer)=>{
+    peerConnection.setLocalDescription(offer);
 
-  let offer = await peerConnection.createOffer();
-  await peerConnection.setLocalDescription(offer);
-  console.log(offer);
+    var message = {offer: offer};
+    socket.emit("message", message);
+    // console.log(offer);
+  });
 };
+
+  socket.on("message", (message) => {
+    if (message.answer) {
+      const remoDesc = new RTCSessionDescription(message.answer);
+      await peerConnection.setRemoteDescription(remoDesc);
+    }
+
+    // when a client recieves an offer we first set the remote as offer and send the answer and set local as answer
+    else if (message.offer) {
+      const remoteDesc = new RTCSessionDescription(message.offer);
+      await peerConnection.setRemoteDescription(remoteDesc);
+
+      //to create an answer and send it.
+      await peerConnection.createAnswer((answer)=>{
+        peerConnection.setLocalDescription(answer);
+        var message = {answer: answer};
+        socket.emit("message", message);
+      });
+    }
+  });
+
+  // handling ice candidates passed
+  socket.on("candidate", (candidate) => {
+    peerConnection.addIceCandidate(new RTCIceCandidate(candidate));
+  });
