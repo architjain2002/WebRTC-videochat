445c39ce9401e74ba6829b6fbd722521e32fd3a3
Archit Jain <arcyjain2002@gmail.com>
2022-07-12 18:20:31 +0530

    create or join event

--- frontend/rooms.js
+++ frontend/rooms.js
@@ -1,6 +1,11 @@
 room = prompt("Enter your room name:");
 
-const socket = io("http://localhost:80"); // this will connect to backend which is currently being server on port 80
+const socket = io("http://127.0.0.1:80"); // this will connect to backend which is currently being server on port 80
 socket.on("connect", () => {
   console.log(socket.id);
 });
+
+if (room !== "") {
+  console.log("Room name: " + room);
+  socket.emit("create or join", room);
+}
