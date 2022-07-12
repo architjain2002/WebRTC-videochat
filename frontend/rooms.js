09c9d0aa8ec6588be31ce46ad161fe0e44851f5b
Archit Jain <arcyjain2002@gmail.com>
2022-07-12 22:19:24 +0530

    set up of room for 2 person complete

--- frontend/rooms.js
+++ frontend/rooms.js
@@ -9,3 +9,19 @@ if (room !== "") {
   console.log("Room name: " + room);
   socket.emit("create or join", room);
 }
+
+socket.on("full", (room) => {
+  console.log("This room is full: " + room);
+});
+
+socket.on("created", (room) => {
+  console.log("Room created: " + room);
+});
+
+socket.on("joined", (room) => {
+  console.log("you joined room: " + room);
+});
+
+socket.on("join", (room, id) => {
+  console.log("socket id: " + id + " joined room: " + room);
+});
